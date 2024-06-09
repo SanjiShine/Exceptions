a26b4309fdbb7f86e663c9ece5a8a7921e4246b0
Sanji <chokpichit@poonpin.de>
2024-06-09 17:33:46 +0200

    MeinFehler.cs and Program.cs first commited

---
+++ Aufgabe01-MeinFehler/MeinFehler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Aufgabe01_MeinFehler
+{
+	public class MeinFehler: SystemException
+	{
+		public int Fehlercode { get; set; }
+
+		public MeinFehler(string message, int fehlercode): base(message)
+		{
+			this.Fehlercode = fehlercode;
+		}
+	}
+}
+
---
+++ Aufgabe01-MeinFehler/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Aufgabe01_MeinFehler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("*******************************");
+            Methode1();
+            Console.WriteLine("*******************************");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Hier ist noch etwas schief gegangen");
+        }
+    }
+
+    static void Methode1()
+    {
+        try
+        {
+            Methode2();
+        }
+        catch (MeinFehler e)
+        {
+            Console.WriteLine($"Message: {e.Message}\nFehlercode:{e.Fehlercode}");
+
+        } catch(SystemException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            // Nicht zu tun
+        }
+    }
+
+    static void Methode2()
+    {
+        try
+        {
+            Methode3();
+        }
+        catch (MeinFehler e)
+        {
+            Console.WriteLine($"Message: {e.Message}\nFehlercode:{e.Fehlercode}");
+            e.Fehlercode = 24;
+            throw e;
+
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw e; 
+        }
+
+        finally
+        {
+            //Nicht zu tun
+        }
+    }
+
+    static void Methode3()
+    {
+        throw new MeinFehler("Error in method 3",  123);
+    }
+
+
+}
+
