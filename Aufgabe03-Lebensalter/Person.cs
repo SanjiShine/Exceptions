0e9768fbe03d1e8e8661bf13ddfa9657203181fb
Sanji <chokpichit@poonpin.de>
2024-06-09 17:35:18 +0200

    Person and Program first commited

---
+++ Aufgabe03-Lebensalter/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Aufgabe03_Lebensalter
+{
+	public class Person
+	{
+		public string name; 
+		public int alter;
+		
+		public Person(string name, int alter)
+		{
+			this.name = name;
+            if (Valid(alter)) this.alter = alter;
+            else throw new Exception("Ungültigen Wert, dir Grenze des gültigen Wertes ist 0 bis 120.");
+        }
+
+		static bool Valid(int alter)
+		{
+			if(alter >= 0 && alter <= 120)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
---
+++ Aufgabe03-Lebensalter/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Aufgabe03_Lebensalter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Person john = new Person("John", 130);
+            Console.WriteLine("Objekt wurde erstellt!");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            Person alex = new Person("Alex", -2);
+            Console.WriteLine("Objekt wurde erstellt!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            Person max = new Person("Max", 0);
+            Console.WriteLine("Objekt wurde erstellt!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            Person mix = new Person("Mix", 120);
+            Console.WriteLine("Objekt wurde erstellt!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        { 
+            Person six = new Person("Six", 50);
+            Console.WriteLine("Objekt wurde erstellt!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
